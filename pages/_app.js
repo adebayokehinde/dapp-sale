9af9a6a780f46ec4acff8a54c7599853d01242f4
Yeppman <40640334+Yeppman@users.noreply.github.com>
2021-08-08 19:52:03 +0100

    added redux for crypto prices

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import '../styles/globals.css'
+import { wrapper } from "../redux/store";
+import { useDispatch } from "react-redux";
+import {GetCryptoPrices ,GetCryptoPricesInDollar} from '../redux/actions/cryptoPrices'
 
 function MyApp({ Component, pageProps }) {
+
+  const dispatch = useDispatch();
+  dispatch(GetCryptoPrices())
+  dispatch(GetCryptoPricesInDollar())
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
